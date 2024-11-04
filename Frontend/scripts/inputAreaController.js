dfb2bfb1c0d743bb94e2ae3da62f5ce2692e3b25
Mohit N. Vashisth <mohitnarayanvashisth2703@gmail.com>
2024-11-04 21:53:09 +0530

    microphone added

--- Frontend/scripts/inputAreaController.js
+++ Frontend/scripts/inputAreaController.js
@@ -1,4 +1,6 @@
-const inputArea = document.querySelector('.input-area');
+const inputArea = document.querySelector('.askModel_response');
+inputArea.style.overflow = 'auto'; // Set overflow to auto
+
 inputArea.addEventListener('input', function () {
     this.style.height = 'auto';
     this.style.height = this.scrollHeight + 'px';
