25e7310e51768acba124609e48ef30a0f632789b
Mohit N. Vashisth <mohitnarayanvashisth2703@gmail.com>
2024-11-06 22:47:52 +0530

    chatHistory recreated

--- Frontend/scripts/chatMenuOptions.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('.chatMenuHolder img').addEventListener('click', function(){
-  this.style.visibility = 'visible';
-  document.querySelector('.chatHistory').style.pointerEvents = 'none';
-  document.querySelector('.chatMenuOptions').style.visibility = 'visible';
-  document.querySelector('.chatDetails').style.filter = 'blur(.5vmin)'; 
-});
