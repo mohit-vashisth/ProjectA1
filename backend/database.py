299321b9ec97ec34aedd74ffbe6674298c371cd1
Mohit N. Vashisth <mohitnarayanvashisth2703@gmail.com>
2024-11-20 23:06:23 +0530

    d

--- backend/database.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-from datetime import datetime
-
-def get_database():
-    # connecting to mongodb
-    client = MongoClient("mongodb://3Rminds:27017/")
-    db = client["3Rminds_db"]
-    return db
