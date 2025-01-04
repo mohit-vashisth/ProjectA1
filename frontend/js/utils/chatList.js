f77c8a881d79cd017af90bb8282689efb5a594b3
Mohit N. Vashisth <mohitnarayanvashisth2703@gmail.com>
2025-01-04 10:55:33 +0530

    Ui without responsive

--- frontend/js/utils/chatList.js
+++ frontend/js/utils/chatList.js
@@ -1,18 +1,21 @@
 const recentChatsButton = document.querySelector('.recentChatsButton');
 const chatsSection = document.querySelector('.chatsSection');
+const iconRotate = document.querySelector('.dropdownRecentChats img');
 
-function openCloseChatList(button, display) {
-    if (button && display) {
+function openCloseChatList(button, display, iconRotate) {
+    if (button && display && iconRotate) {
         button.addEventListener('click', function () {
             if (!display.classList.contains('openChatListDisplay')) {
                 button.classList.add('openChatList');
                 display.classList.add('openChatListDisplay');
+                iconRotate.style.transform = 'rotate(180deg)';
             } else {
                 button.classList.remove('openChatList');
                 display.classList.remove('openChatListDisplay');
+                iconRotate.style.transform = 'rotate(0deg)';
             }
         });
     }
 }
 
-openCloseChatList(recentChatsButton, chatsSection);
+openCloseChatList(recentChatsButton, chatsSection, iconRotate);
--- frontend/js/utils/screenSlider.js
+++ frontend/js/utils/screenSlider.js
@@ -1,18 +1,21 @@
 const slideBar = document.querySelector('.slider');
 const leftSection = document.querySelector('.leftMain');
 const rightSection = document.querySelector('.rightMain');
+const upperRight = document.querySelector('.upperRight');
 
-function chatSlider(sliderIcon, leftElement, rightElement) {
+function chatSlider(sliderIcon, leftElement, rightElement, navBar) {
   slideBar.addEventListener('click', function(){
     if(leftElement.style.width != '0%'){
       leftElement.style.width = '0%';
       rightElement.style.width = '100%';
+      navBar.style.width = '100%';
     }
     else{
       leftElement.style.width = '13%';
       rightElement.style.width = '87%';
+      navBar.style.width = '87%';
     }
   })
 }
 
-chatSlider(slideBar, leftSection, rightSection);
+chatSlider(slideBar, leftSection, rightSection, upperRight);
