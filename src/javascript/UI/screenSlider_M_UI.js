efe677386462288befa2f884d3abce512d9f6ce2
Mohit N. Vashisth <mohitnarayanvashisth2703@gmail.com>
2025-03-17 21:14:10 +0530

    website optimized for phone

--- src/javascript/UI/screenSlider_M_UI.js
+++ src/javascript/UI/screenSlider_M_UI.js
@@ -1,12 +1,29 @@
 const slideBar = document.querySelector('.slider');
 const leftSection = document.querySelector('.leftMain');
+const rightSection = document.querySelector('.rightMain');
+const closeCross = document.querySelector('.closeLeftMainContainer img');
 
 export function slideMobileEXP() {
     slideBar.addEventListener("click", ()=>{
         if(leftSection.style.width === "100%"){
             leftSection.style.width = "0%";
+            rightSection.style.pointerEvents = "auto"; 
+            rightSection.style.opacity = 1;
         } else{
             leftSection.style.width = "100%"
+            rightSection.style.pointerEvents = "none"; 
+            rightSection.style.opacity = 0.1;
+        }
+    })
+    closeCross.addEventListener("click", ()=>{
+        if(leftSection.style.width === "100%"){
+            leftSection.style.width = "0%";
+            rightSection.style.pointerEvents = "auto"; 
+            rightSection.style.opacity = 1;
+        } else{
+            leftSection.style.width = "100%"
+            rightSection.style.pointerEvents = "none"; 
+            rightSection.style.opacity = 0.1;
         }
     })
 }
